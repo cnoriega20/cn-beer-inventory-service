e5ff696f6d1947ee4ba1a777c1d24e5ca1b8d583
cnoriega20 <c79.noriega@gmail.com>
2020-10-14 10:40:45 -0500

    Initial commit: Spring intializr project structure.

---
+++ src/main/java/tn/beer/inventory/CnBeerInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package tn.beer.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnBeerInventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnBeerInventoryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/beer/inventory/CnBeerInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.beer.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnBeerInventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
